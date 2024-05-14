7d6139d368cb519ba95412fa20b6866867603c12
TNoLisme <167105259+TNoLisme@users.noreply.github.com>
2024-05-15 00:05:45 +0700

    Update Score.cpp

--- Score.cpp
+++ Score.cpp
@@ -10,28 +10,17 @@ Score::Score(SDL_Color color, int level, int score, int timeClick, int x, int y,
     this->rect = { 0, 0, x, y };
     this->rectScore = { 0, x, x, y - x };
     this->font = TTF_OpenFont("image/game.ttf", 80);
-    if (!font) {
-        std::cerr << "Failed to load font! SDL_ttf Error: " << TTF_GetError() << std::endl;
-    }
-    update(level, score, timeClick);
+
 }
 
 Score::~Score() {
     TTF_CloseFont(font);
 }
 
-void Score::update(int level, int score, int timeClick) {
-    this->level = level;
-    this->score = score;
-    this->timeClick = timeClick;
-    noti = std::to_string(level) + "  " + std::to_string(score) + "  " + std::to_string(timeClick);
-}
-
 void Score::render(SDL_Renderer* renderer) {
     SDL_Surface* surface = TTF_RenderText_Solid(font, noti.c_str(), color);
     SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
     SDL_FreeSurface(surface);
-
     SDL_RenderCopy(renderer, texture, nullptr, &rect);
     SDL_DestroyTexture(texture);
 }
